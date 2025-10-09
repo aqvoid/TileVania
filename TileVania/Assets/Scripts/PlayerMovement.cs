c0c0b4af6a4c19a285aea558bf988d9775b7e246
aqvoidDev <114750046+aqvoid@users.noreply.github.com>
2025-10-09 20:40:38 +0300

    Added Basic Player Movement
    - set player input system

---
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float runSpeed = 5;
+    [SerializeField] private float jumpForce = 5;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Run();
+        Jump();
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = playerVelocity;
+    }
+
+    private void Jump()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            //Run();
+            //Jump();
+        }
+    }
+}
