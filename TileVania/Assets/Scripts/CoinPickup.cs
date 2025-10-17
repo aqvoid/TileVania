6e8e5f2ccae6cb8bda4afe36bb0515c4ec5ffa3c
aqvoidDev <mindeow35@gmail.com>
2025-10-17 19:14:06 +0300

    Added Coin Sound Effect

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] AudioClip coinPickupSFX;
+
+    private bool wasCollected = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player") && !wasCollected)
+        {
+            wasCollected = true;
+            AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
             Destroy(gameObject);
+        }
     }
 
 }
