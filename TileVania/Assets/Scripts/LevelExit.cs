5c53ea3d548f1579626400f2e8768ecbfd1c4120
aqvoidDev <mindeow35@gmail.com>
2025-10-18 17:48:37 +0300

    Added Looping Progression between Levels

--- TileVania/Assets/Scripts/LevelExit.cs
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -1,13 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [SerializeField] private float levelLoadDelay = 1f;
     private int currentLevelIndex;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+
         currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentLevelIndex + 1);
+        int nextSceneIndex = currentLevelIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) nextSceneIndex = 0;
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
