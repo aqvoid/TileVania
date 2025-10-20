87f70cc5d31319467a13c1dc440340cd231ef775
aqvoidDev <mindeow35@gmail.com>
2025-10-20 21:22:17 +0300

    Added Slime & Coin Prefab Variants
    - now you'll also get points from slimes

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] private AudioClip coinPickupSFX;
-    [SerializeField] private const int POINTS_ADD_FOR_PICKUP = 100;
+    [SerializeField] private int pointAddForPickup = 100;
 
     private bool wasCollected = false;
 
@@ -12,7 +12,7 @@ public class CoinPickup : MonoBehaviour
         if (other.CompareTag("Player") && !wasCollected)
         {
             wasCollected = true;
-            FindFirstObjectByType<GameSession>().AddToScore(POINTS_ADD_FOR_PICKUP);
+            FindFirstObjectByType<GameSession>().AddToScore(pointAddForPickup);
             AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
             Destroy(gameObject);
         }
