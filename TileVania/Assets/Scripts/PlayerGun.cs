9e8d0b1f44489fe451d5dbf141116e3c160643e1
aqvoidDev <mindeow35@gmail.com>
2025-10-22 00:21:16 +0300

    Added Gun Cooldown & Auto Attack

--- TileVania/Assets/Scripts/PlayerGun.cs
+++ TileVania/Assets/Scripts/PlayerGun.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,42 +6,93 @@ public class PlayerGun : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
     [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private float bulletCooldown = 1f;
 
     private PlayerMortality playerMortality;
     private PlayerMovement playerMovement;
 
+    private Vector2 mousePos;
+    private Vector2 direction;
+    private bool isShooting = false;
+    private bool canShoot = true;
+
     private PlayerInput playerInput;
     private InputAction attackAction;
-
 
     private void Awake()
     {
         playerMortality = GetComponentInParent<PlayerMortality>();
         playerInput = GetComponentInParent<PlayerInput>();
         playerMovement = GetComponentInParent<PlayerMovement>();
+
         attackAction = playerInput.actions["Attack"];
-        attackAction.performed += OnAttack;
     }
 
-    private void OnDestroy() => attackAction.performed -= OnAttack;
+    private void OnEnable()
+    {
+        attackAction.started += OnAttackStarted;
+        attackAction.canceled += OnAttackCanceled;
+    }
 
-    private void OnAttack(InputAction.CallbackContext context)
+    private void OnDisable()
+    {
+        attackAction.started -= OnAttackStarted;
+        attackAction.canceled -= OnAttackCanceled;
+    }
+
+    private void OnAttackStarted(InputAction.CallbackContext context)
     {
         if (!playerMortality.IsAlive) return;
+        if (isShooting) return;
+        isShooting = true;
+        StartCoroutine(AutoShoot());
+    }
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+    private void OnAttackCanceled(InputAction.CallbackContext context)
+    {
+        isShooting = false;
+    }
 
-        if (direction.x != 0)
+    private IEnumerator AutoShoot()
+    {
+        while (isShooting)
         {
-            float facing = Mathf.Sign(direction.x);
-            transform.parent.localScale = new Vector2(Mathf.Abs(transform.parent.localScale.x) * facing, 1f);
-        }
+            if (canShoot)
+            {
+                canShoot = false;
 
+                SaveShootPosition();
+                ChangeFacingDirection();
+                CreateBullet();
+
+                yield return new WaitForSeconds(bulletCooldown);
+                canShoot = true;
+            }
+            yield return null;
+        }
+    }
+
+    private void CreateBullet()
+    {
         GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
         Rigidbody2D bulletRb = newBullet.GetComponent<Rigidbody2D>();
 
         bulletRb.linearVelocity = direction * bulletSpeed + playerMovement.GetVelocity();
         bulletRb.angularVelocity = Random.Range(-180, 180);
+    }
+
+    private void ChangeFacingDirection()
+    {
+        if (direction.x != 0)
+        {
+            float facing = Mathf.Sign(direction.x);
+            transform.parent.localScale = new Vector2(Mathf.Abs(transform.parent.localScale.x) * facing, 1f);
+        }
+    }
+
+    private void SaveShootPosition()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        direction = (mousePos - (Vector2)transform.position).normalized;
     }
 }
